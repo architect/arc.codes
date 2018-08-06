5cf1ab008703b6721fc424ba70b1ae6080aa909e
Francis Gulotta <wizard@roborooter.com>
2018-08-06 14:56:42 -0400

    Show off our favicons

--- src/shared/render.js
+++ src/shared/render.js
@@ -36,6 +36,9 @@ module.exports = function render (filename) {
           <meta charset=UTF-8>
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" type="text/css">
+          <link rel="icon" type="image/png" sizes="32x32" href="https://s3-us-west-2.amazonaws.com/arc.codes/favicon-32.png">
+          <link rel="icon" type="image/png" sizes="16x16" href="https://s3-us-west-2.amazonaws.com/arc.codes/favicon-16.png">
+          <link rel="icon" type="image/png" sizes="64x64" href="https://s3-us-west-2.amazonaws.com/arc.codes/favicon-64.png">
           <style type=text/css>${style}</style>
         </head>
         <body class="${classes}">
