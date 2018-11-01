907800ac3af85e5a3e94663c00fe4732e105dc0a
Ryan Block <rblock@gmail.com>
2018-10-31 21:00:28 -0700

    Well, at least progress is consistently failing across commands in travis

--- scripts/deploy-travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# travis script deploy only accepts one script
-# travis after_success won't deploy conditionally
-# so we're doing this ಠ_ಠ
-mkdir ~/.aws
-printf "[jsf]\naws_access_key_id=${AWS_ACCESS_KEY_ID}\naws_secret_access_key=${AWS_SECRET_ACCESS_KEY}\nregion=us-west-2\n" > ~/.aws/credentials && 
-npx deploy
-rm -rf ~/.aws
