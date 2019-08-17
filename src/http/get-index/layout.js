d5d0a649d4d3a5716b4511f0fdda982f35443ee6
Mike MacCana <mike.maccana@gmail.com>
2019-08-17 17:59:14 +0100

    Add language switcher for code examples

--- src/http/get-index/layout.js
+++ src/http/get-index/layout.js
@@ -63,6 +63,7 @@ module.exports = function layout(filename, {headers, body}) {
 </script>
 
 <script type=module src=/_static/nav.js></script>
+<script type=module src=/_static/code-examples.js></script>
 <script type=module src=/_static/arcfile.js></script>
 <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-74655805-3', 'auto');ga('send', 'pageview');
