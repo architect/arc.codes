8f1b178c2d88bb33e537d6d20b0fa22352cbf81f
Brian LeRoux <b@brian.io>
2019-11-19 14:55:47 -0800

    send em to the home repo

--- src/http/get-index/layout.js
+++ src/http/get-index/layout.js
@@ -44,7 +44,7 @@ module.exports = function layout(filename, {headers, body}) {
         </footer>
       </section>
     </section>
-    <a href="https://github.com/arc-repos/arc.codes" class="github-corner" aria-label="View source on Github">${github}</a>
+    <a href="https://github.com/architect/architect" class="github-corner" aria-label="View source on Github">${github}</a>
 <style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>
 <script>
 (function() {
