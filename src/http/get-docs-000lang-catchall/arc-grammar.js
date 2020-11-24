d90e08c72a89b279cbab2b0c152f823965eab6f3
kj <kristoferjoseph@gmail.com>
2020-11-24 10:07:15 -0800

    Grammar update.

--- src/http/get-docs-000lang-catchall/arc-grammar.js
+++ src/http/get-docs-000lang-catchall/arc-grammar.js
@@ -1,46 +1,49 @@
 module.exports = function(hljs) {
     return {
+      aliases: [
+        'arc',
+        'architect'
+      ],
       case_insensitive: true,
       builtins: [
-          "@app",
-          "@aws",
-          "@cdn",
-          "@events",
-          "@http",
-          "@indexes",
-          "@proxy",
-          "@queues",
-          "@scheduled",
-          "@static",
-          "@tables",
-          "@ws"
+        "@app",
+        "@aws",
+        "@cdn",
+        "@events",
+        "@http",
+        "@indexes",
+        "@proxy",
+        "@queues",
+        "@scheduled",
+        "@static",
+        "@tables",
+        "@ws"
       ].join(' '),
       keywords: [
-          "get",
-          "post",
-          "put",
-          "patch",
-          "delete",
-          "options",
-          "head",
-          "any",
-          "region",
-          "profile",
-          "runtime",
-          "bucket",
-          "http",
-          "httpv1",
-          "rest",
-          "apigateway",
-          "fingerprint",
-          "folder"
+        "get",
+        "post",
+        "put",
+        "patch",
+        "delete",
+        "options",
+        "head",
+        "any",
+        "region",
+        "profile",
+        "runtime",
+        "bucket",
+        "httpv1",
+        "rest",
+        "apigateway",
+        "fingerprint",
+        "folder"
       ].join(' '),
       contains: [
         {
           className: 'string',
           begin: "'", end: "'"
         },
-        hljs.COMMENT(
+      hljs.COMMENT(
           '#', // begin
           'null', // end
         )
