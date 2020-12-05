dca29f66672fc5dbe6ff9c1e873edcdcd760a25b
kj <kristoferjoseph@gmail.com>
2020-12-05 15:08:15 -0800

    Adds read percentage to header.

--- public/index.js
+++ public/index.js
@@ -1,10 +1,13 @@
  (function(){
+    // Scroll active sidebar link into view
     document
        .querySelector('a.active')
        .scrollIntoView({
          behavior: 'smooth',
          block:  'center'
         })
+
+    // Toggle sidebar on mobile
     let menuButton = document
        .getElementById('menu-button')
     let sidebar = document
@@ -19,4 +22,24 @@
     menuButton.onclick = function toggleSidebar() {
       sidebar.classList.toggle('open')
     }
+
+    // Show percentage of page read
+    let bar = document.querySelector('.hairline')
+    bar.style.width = getScrollPercent(main)
+    main.onscroll = setReadPercent
+
+    function setReadPercent() {
+      bar.style.width = `${getScrollPercent(main)}%`
+    }
+
+    function getScrollPercent(el) {
+      let body = document.body
+      let scrollTop = 'scrollTop'
+      let scrollHeight = 'scrollHeight'
+      let currentTop = el[scrollTop] || body[scrollTop]
+      let currentHeight = (el[scrollHeight] || body[scrollHeight]) - body.clientHeight
+      return Math.floor((currentTop / currentHeight) * 100);
+    }
+
+
  }())
