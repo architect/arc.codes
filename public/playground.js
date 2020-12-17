35415395eaa8a8f3599709b9cabdc2db06dea370
kj <kristoferjoseph@gmail.com>
2020-12-16 16:36:24 -0800

    Updates playground.

--- public/playground.js
+++ public/playground.js
@@ -1,53 +1,42 @@
-init()
-
-function init() {
-  initForm()
-  initButton()
-  initInput()
-  initShare()
-}
-
-function initButton() {
+(function() {
   let btn = document.getElementById('pkg-submit')
   btn.remove()
-}
 
-function initForm() {
   const form = document.getElementById('pkg-form')
   form.onsubmit = submit
-}
 
-function initShare() {
   const shareBtn = document.getElementById('pkg-share')
   shareBtn.onclick = copyShare
-}
 
-function initInput() {
-  let input = document.getElementById('pkg-input')
+  const input = document.getElementById('pkg-input')
+  input.addEventListener('input', getPreview)
+
   let query = new URLSearchParams(window.location.search)
   let arc = query.get('arc')
   if (!arc) {
     arc = `@app\nmyapp\n\n@http\nget /`
   }
-  input.addEventListener('input', getPreview)
+
   input.value = arc
   getPreview()
-}
+}())
+
 
 function copyShare(e) {
   e.preventDefault()
-  const shareBtn = document.getElementById('pkg-share')
+  let shareBtn = e.target
   let input = document.getElementById('pkg-input')
   let arc = input.value
-  const shareUrl = `${window.location}?arc=${encodeURIComponent(arc)}`
+  let shareUrl = `${window.location}?arc=${encodeURIComponent(arc)}`
   navigator.clipboard.writeText(shareUrl)
   shareBtn.innerHTML = 'Copied to clipboard'
-  setTimeout(function resetButton () {
+  setTimeout(function resetButton() {
     shareBtn.innerHTML = 'Share'
   }, 2000)
 }
 
 async function getPreview() {
+  console.log('GET PREVIEW')
   const input = document.getElementById('pkg-input')
   const arc = input.value
   const url = `/api/1/package?arc=${encodeURIComponent(arc)}`
