f748493c95e0110c8cdf5d28579a0127554a70b4
ShawnHosea <shawnhosea@shawnhosea.com>
2021-01-22 04:36:14 -0500

    metatag fix

--- src/views/modules/document/head.js
+++ src/views/modules/document/head.js
@@ -13,29 +13,27 @@ export default function Head (props = {}) {
   <meta name="description" content="${descriptionContent}">
   <title>${fullTitle}</title>
 
-  <!-- Open Graph / Facebook -->
-  <meta property="og:type" content="website">
-  <meta property="og:url" content="https://arc.codes/docs/en/guides/get-started/quickstart">
-  <meta property="og:title" content="OpenJS Architect documentation">
-  <meta property="og:description" content="Architect is the quickest way to build serverless web apps on AWS">
+<!-- Google / Search Engine Tags -->
+  <meta itemprop="name" content="Architect">
+  <meta itemprop="description" content="Architect is the quickest way to build serverless web apps on AWS.">
+  <meta itemprop="image" content="https://arc.codes/arc.codes.png">
 
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://arc.codes">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="OpenJS Architect">
+  <meta property="og:description" content="Architect is the quickest way to build serverless web apps on AWS.">
+  <!-- TODO: Update image -->
   <meta property="og:image" content="https://arc.codes/arc.codes.png">
 
-  <!-- Twitter -->
-  <meta property="twitter:card" content="summary_large_image">
-  <meta property="twitter:url" content="https://arc.codes/docs/en/guides/get-started/quickstart">
-  <meta property="twitter:title" content="OpenJS Architect documentation">
-  <meta property="twitter:description" content="Architect is the quickest way to build serverless web apps on AWS">
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content="OpenJS Architect">
+  <meta name="twitter:description" content="Architect is the quickest way to build serverless web apps on AWS.">
+  <!-- TODO: Update image -->
+  <meta name="twitter:image" content="https://arc.codes/arc.codes.png">
 
-  <meta property="twitter:image" content="">
-
-  <!-- OG Begin -->
-  <link rel="icon" type="image/png" sizes="32x32" href="https://s3-us-west-2.amazonaws.com/arc.codes/architect-favicon-32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="https://s3-us-west-2.amazonaws.com/arc.codes/architect-favicon-16.png">
-  <link rel="icon" type="image/png" sizes="64x64" href="https://s3-us-west-2.amazonaws.com/arc.codes/architect-favicon-64.png">
-  <link rel="stylesheet" type="text/css" href="/css/styles.css">
-  <link rel="stylesheet" type="text/css" href="/css/index.css">
-  <link rel="stylesheet" type="text/css" href="/css/syntax.css">
+  <!-- Meta Tags end-->
 </head>
 `
 }
