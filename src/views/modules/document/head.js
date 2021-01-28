144041b410b4a6169e73c16ba7a167c5e99a9965
ShawnHosea <shawnhosea@shawnhosea.com>
2021-01-28 18:32:08 -0500

    Fixed metatags completely

--- src/views/modules/document/head.js
+++ src/views/modules/document/head.js
@@ -7,39 +7,44 @@ export default function Head (props = {}) {
 
   return `
 <head>
-  <!-- Primary Meta Tags -->
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1">
-  <meta name="description" content="${descriptionContent}">
-  <title>${fullTitle}</title>
+<!-- Primary Meta Tags -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1">
+<meta name="description" content="${descriptionContent}">
+<title>${fullTitle}</title>
 
-  <!-- Google / Search Engine Tags -->
-  <meta itemprop="name" content="Architect">
-  <meta itemprop="description" content="Architect is the quickest way to build serverless web apps on AWS.">
-  <meta itemprop="image" content="https://arc.codes/arc.codes.png">
+<!-- Search Engine -->
+<meta name="description" content="Architect is the quickest way to build serverless web apps on AWS.">
+<meta name="image" content="https://arc.codes/arc.codes.png">
 
-  <!-- Facebook Meta Tags -->
-  <meta property="og:url" content="https://arc.codes">
-  <meta property="og:type" content="website">
-  <meta property="og:title" content="OpenJS Architect">
-  <meta property="og:description" content="Architect is the quickest way to build serverless web apps on AWS.">
-  <!-- TODO: Update image -->
-  <meta property="og:image" content="https://arc.codes/arc.codes.png">
+<!-- Schema.org for Google -->
+<meta itemprop="name" content="Architect documentation">
+<meta itemprop="description" content="Architect is the quickest way to build serverless web apps on AWS.">
+<meta itemprop="image" content="https://arc.codes/arc.codes.png">
 
-  <!-- Twitter Meta Tags -->
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content="OpenJS Architect">
-  <meta name="twitter:description" content="Architect is the quickest way to build serverless web apps on AWS.">
-  <!-- TODO: Update image -->
-  <meta name="twitter:image" content="https://arc.codes/arc.codes.png">
+<!-- Twitter -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Architect documentation">
+<meta name="twitter:description" content="Architect is the quickest way to build serverless web apps on AWS.">
+<meta name="twitter:site" content="@begin">
+<meta name="twitter:creator" content="@begin">
+<meta name="twitter:image:src" content="https://arc.codes/arc.codes.png">
 
-  <!-- OG Begin -->
-  <link rel="icon" type="image/png" sizes="32x32" href="https://s3-us-west-2.amazonaws.com/arc.codes/architect-favicon-32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="https://s3-us-west-2.amazonaws.com/arc.codes/architect-favicon-16.png">
-  <link rel="icon" type="image/png" sizes="64x64" href="https://s3-us-west-2.amazonaws.com/arc.codes/architect-favicon-64.png">
-  <link rel="stylesheet" type="text/css" href="/css/styles.css">
-  <link rel="stylesheet" type="text/css" href="/css/index.css">
-  <link rel="stylesheet" type="text/css" href="/css/syntax.css">
+<!-- Open Graph general (Facebook, Pinterest & Google+) -->
+<meta name="og:title" content="Architect documentation">
+<meta name="og:description" content="Architect is the quickest way to build serverless web apps on AWS.">
+<meta name="og:image" content="https://arc.codes/arc.codes.png">
+<meta name="og:url" content="https://arc.codes/">
+<meta name="og:site_name" content="Architect documentation">
+<meta name="og:type" content="website">
+
+<!-- Styles/Favicons -->
+<link rel="icon" type="image/png" sizes="32x32" href="https://s3-us-west-2.amazonaws.com/arc.codes/architect-favicon-32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://s3-us-west-2.amazonaws.com/arc.codes/architect-favicon-16.png">
+<link rel="icon" type="image/png" sizes="64x64" href="https://s3-us-west-2.amazonaws.com/arc.codes/architect-favicon-64.png">
+<link rel="stylesheet" type="text/css" href="/css/styles.css">
+<link rel="stylesheet" type="text/css" href="/css/index.css">
+<link rel="stylesheet" type="text/css" href="/css/syntax.css">
 </head>
 `
 }
