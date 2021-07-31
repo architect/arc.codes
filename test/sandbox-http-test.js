f9cd9eeafa1f5cd66a3f4169013bf85aaca4e9c0
Taylor Beseda <tbeseda@gmail.com>
2021-07-31 14:37:33 -0600

    add http smoke test

---
+++ test/sandbox-http-test.js
@@ -0,0 +1,21 @@
+let test = require('tape')
+let tiny = require('tiny-json-http')
+let sandbox = require('@architect/sandbox')
+
+const host = 'http://localhost:3333'
+
+test('sandbox HTTP request', async t => {
+  t.plan(4)
+
+  await sandbox.start({ quiet: true })
+  t.ok(true, `sandbox started on ${host}`)
+
+  let quickstart = await tiny.get({ url: `${host}/docs/en/guides/get-started/quickstart` })
+  t.ok(quickstart.body, 'got quickstart document')
+
+  let playground = await tiny.get({ url: `${host}/playground` })
+  t.ok(playground.body, 'got static playground document')
+
+  await sandbox.end()
+  t.ok(true, 'sandbox ended')
+})
