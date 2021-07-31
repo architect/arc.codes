1c8f86b679ae58d11ea08e80294fa93c19898f0d
Taylor Beseda <tbeseda@gmail.com>
2021-07-31 13:24:23 -0600

    use @architect/asap to proxy static playground
    in lieu of @architect/functions http.proxy
    
    this became an issue when dependabot updated functions to 4.0.0

--- src/http/any-catchall/index.js
+++ src/http/any-catchall/index.js
@@ -1,14 +1,15 @@
 let { http } = require('@architect/functions')
+let asap = require('@architect/asap')
 
 // middleware to preserve old urls
 let redirect = require('./redirect')
 
 // middleware proxy s3 assets
-let asap = http.proxy({
+let static = asap({
   spa: false,
   alias: {
     '/playground': '/playground.html'
   }
 })
 
-exports.handler = http.async(redirect, asap)
+exports.handler = http.async(redirect, static)
