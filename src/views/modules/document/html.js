a0d08242bed62b0f93f1d991968dbc19e14c6fea
Taylor Beseda <tbeseda@gmail.com>
2021-09-24 12:21:33 -0600

    create TopNav and adjust mobile padding

--- src/views/modules/document/html.js
+++ src/views/modules/document/html.js
@@ -1,14 +1,9 @@
 import Head from './head.js'
 import Symbols from './symbols.js'
 import Script from './script.js'
+import TopNav from './top-nav.js'
 import State from './state.js'
-import Logo from '../components/logo.js'
-import Icon from '../components/icon.js'
 import Sidebar from '../components/sidebar.js'
-import Search from '../components/search.js'
-import GithubLink from '../components/github-link.js'
-import DiscordLink from '../components/discord-link.js'
-import ThemeButton from '../components/theme-button.js'
 
 export default function HTML (props = {}) {
   let {
@@ -46,89 +41,7 @@ ${Symbols}
       two-column
     "
   >
-    <header
-      class="
-        pt-1
-        pr2
-        pb0
-        pl2
-        sticky
-        relative-lg
-        flex
-        items-center
-        justify-between
-        top0
-        bg-g9
-        col-start-1
-        col-end-3
-        text-g0
-      "
-    >
-      <a
-        aria-label="OpenJS Architect"
-        href="/"
-        class="
-          text-g0
-          text-h0
-          text-a2
-          cursor-pointer
-        "
-      >
-        ${Logo({ classes: 'logo' })}
-      </a>
-      <div
-        class="
-          flex
-          items-center
-          justify-between
-        "
-      >
-        ${Search({ classes: 'hidden inline-block-lg mr0' })}
-        ${DiscordLink()}
-        ${GithubLink({ classes: 'ml0' })}
-        ${ThemeButton({ classes: 'ml0' })}
-        <button
-          aria-label="Menu"
-          id="menu-button"
-          class="
-            ml0
-            bg-unset
-            text-g0
-            text-h0
-            text-a2
-            hidden-lg
-            cursor-pointer
-          "
-        >
-          ${Icon({ href: 'hamburger', classes: 'icon fill-current' })}
-        </button>
-      </div>
-      <div class="indicator bg-image0 absolute right0 bottom0 left0"></div>
-    </header>
-    <div
-      class="
-        pt-1
-        pr2
-        pb0
-        pl2
-        flex
-        items-center
-        justify-center
-        top0
-        bg-g1
-        col-start-1
-        col-end-3
-        text-g8
-      "
-    >
-      <span>
-        We've moved the community chat, join us on
-        <a href="https://discord.gg/y5A2eTsCRX"
-          class="font-medium text-p1 text-h1"
-          target="_blank" rel="noopener noreferrer"
-        >Discord</a>
-      </span>
-    </div>
+    ${TopNav()}
     ${Sidebar(props)}
     <main
       id="main"
