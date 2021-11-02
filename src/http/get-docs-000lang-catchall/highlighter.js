19517678397ead44d20a5cbd7df040ef92dab24f
Ryan Block <rblock@gmail.com>
2021-11-01 20:54:22 -0700

    Ditch Highlight.js as a normal dependency, vendor it in and shed 7.4MB

--- src/http/get-docs-000lang-catchall/highlighter.js
+++ src/http/get-docs-000lang-catchall/highlighter.js
@@ -1,6 +1,5 @@
 module.exports = function (hljs, escapeHtml, str, lang) {
   if (lang && hljs.getLanguage(lang)) {
-
     try {
       return `<pre class="hljs mb0 mb1-lg"><code>${hljs.highlight(str, { language: lang, ignoreIllegals: true }).value}</code></pre>`
     }
