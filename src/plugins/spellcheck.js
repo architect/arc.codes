8891a79f4075586ee994af0ca3a357c9d8eff849
Ryan Block <rblock@gmail.com>
2022-02-15 13:59:33 -0800

    Make spellcheck plugin slightly nicer

--- src/plugins/spellcheck.js
+++ src/plugins/spellcheck.js
@@ -1,24 +1,34 @@
+let { updater } = require('@architect/utils')
 let { spawn } = require('child_process')
 let pkg = require('../../package.json')
 let cmd = pkg.scripts.spellcheck.split(' ')
+let update = updater('Spelling')
 
 module.exports = {
   sandbox: {
     watcher: async ({ filename }) => {
       if (!filename.endsWith('.md')) return
       return new Promise((res) => {
-        console.log('Checking spelling...')
+        update.start('Checking spelling')
+        let start = Date.now()
+        let done = () => update.done(`Checked spelling in ${(Date.now() - start) / 1000} seconds`)
         let spell = spawn(cmd[0], cmd.slice(1))
         let found = false
         let log = data => {
-          if (!found) console.log('\nFound spelling or grammar error(s):')
+          if (!found) {
+            done()
+            update.warn(`Found spelling or grammar error(s):`)
+          }
           found = true
           console.log(data.toString())
         }
         spell.stdout.on('data', log)
         spell.stderr.on('data', log)
         spell.on('close', code => {
-          if (!code) console.log('No spelling or grammar errors found!')
+          if (!code) {
+            done()
+            update.done('No spelling or grammar errors found!')
+          }
           // Always resolve, errors should have been printed already
           res()
         })
