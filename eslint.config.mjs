1acf0ff43c509df6845e07eb992e91a12f6991e4
Ryan Block <rblock@gmail.com>
2025-01-02 10:46:00 -0800

    Upgrade eslint to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import arc from '@architect/eslint-config'
+import importPlugin from 'eslint-plugin-import'
+
+export default [
+  ...arc,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      import: importPlugin.flatConfigs.recommended.plugins.import
+    },
+    rules: {
+      'import/no-commonjs': 'error',
+      'import/extensions': [
+        'error',
+        'ignorePackages'
+      ],
+      // Additive to our old `import` config, but everything seems quite sane!
+      ...importPlugin.flatConfigs.recommended.rules,
+    }
+  },
+]
